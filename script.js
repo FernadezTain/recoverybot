c38427c53ca6cefd4a2899ff3a73bd7578d55a19
FernadezTain <fernadezofficial489@gmail.com>
2026-01-21 18:23:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const card = document.querySelector(".card");
+const card = document.getElementById("card");
+const btn = document.getElementById("continueBtn");
 
+// 3D эффект
 card.addEventListener("mousemove", (e) => {
     const rect = card.getBoundingClientRect();
-
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
     const centerX = rect.width / 2;
     const centerY = rect.height / 2;
 
-    const rotateX = ((y - centerY) / centerY) * 10;
-    const rotateY = ((x - centerX) / centerX) * 10;
+    const rotateX = ((y - centerY) / centerY) * 8;
+    const rotateY = ((x - centerX) / centerX) * 8;
 
     card.style.transform = `
         rotateX(${-rotateX}deg)
         rotateY(${rotateY}deg)
     `;
-
-    card.style.setProperty("--x", `${x}px`);
-    card.style.setProperty("--y", `${y}px`);
 });
 
 card.addEventListener("mouseleave", () => {
-    card.style.transform = `
-        rotateX(0deg)
-        rotateY(0deg)
-    `;
+    card.style.transform = `rotateX(0deg) rotateY(0deg)`;
+});
+
+// Переключение
+btn.addEventListener("click", () => {
+    card.classList.add("active");
 });
